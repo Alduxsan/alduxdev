5560cbdde9b089b6f6c5af67aa8a62d24dc7bbbd
Alduxsan <aldosantarelli@gmail.com>
2023-02-12 22:26:20 -0300

    header style changed / mobile styles added

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ function change_closeIcon_visibility(element) {
 function openGrid(tabContent, clickedTab) {
   let tab;
   let header;
+  let content = document.getElementById(tabContent);
   for (tab = 0; tab < tab_content.length; tab++) {
     tab_content[tab].classList.add("hide_content");
     tab_content[tab].classList.remove("show_content");
@@ -20,15 +21,15 @@ function openGrid(tabContent, clickedTab) {
   for (header = 0; header < columns.length; header++) {
     columns[header].classList.remove("selectedTab");
   }
-  document
-    .getElementById(tabContent)
-    .classList.replace("hide_content", "show_content");
+
+  content.classList.replace("hide_content", "show_content");
   clickedTab.classList.toggle("selectedTab");
+  content.scrollIntoView();
 }
 
 function lazy_show() {
   const options = {
-    rootMargin: "0px 0px -200px 0px",
+    rootMargin: "0px 0px -100px 0px",
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
@@ -70,3 +71,36 @@ function toggle_project_info(info_element, button) {
     "close_project_info"
   );
 }
+
+new TypeIt("#main_title", {
+  strings: ["I'm <strong>Aldo</strong>"],
+  speed: 150,
+  cursor: false,
+  loop: false,
+  breakLines: false,
+  startDelete: true,
+  startDelay: 3000,
+  nextStringDelay: 3000,
+}).go();
+
+new TypeIt("#role", {
+  strings: ["frontend developer", "fullstack developer", "web developer"],
+  speed: 100,
+  cursor: false,
+  loop: false,
+  breakLines: false,
+  startDelay: 8000,
+  nextStringDelay: 3000,
+}).go();
+
+new TypeIt("#together", {
+  strings: ["Let's work together!", "Find me at:"],
+  speed: 180,
+  cursor: false,
+  loop: false,
+  breakLines: false,
+  startDelete: true,
+  startDelay: 1000,
+  nextStringDelay: 2000,
+  waitUntilVisible: true,
+}).go();
